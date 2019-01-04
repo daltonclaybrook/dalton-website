09be2cd013be7ab310bba1524129279305e1400d
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-01-03 21:47:54 -0500

    Remove starter project logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Bio from './components/Bio';
 import Books from './components/Books';
-import logo from './logo.svg';
 
 class App extends Component {
   public render() {
@@ -10,22 +9,6 @@ class App extends Component {
       <>
         <Bio />
         <Books />
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
       </>
     );
   }
