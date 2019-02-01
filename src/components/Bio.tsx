ad7f5601024d74ff86ee76e459e661d4baca3e6b
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-01-31 22:22:12 -0500

    Add gravatar img to Bio

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 
 const Bio: FunctionComponent = () => (
     <div className="bio">
+        <img src="https://gravatar.com/avatar/409d9fd7356a2876a35dcd461713d749?s=400" />
         <h1>Dalton Claybrook</h1>
         <p>Thanks for checking out my site.</p>
     </div>
