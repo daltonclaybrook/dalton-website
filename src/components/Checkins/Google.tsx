5ab36ad687b198bd5a5b7027d1e89501aaec19fa
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-02-07 09:04:11 -0500

    Fetching places again

--- src/components/Checkins/Google.tsx
+++ src/components/Checkins/Google.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Checkin from '../../models/Checkin';
+import CheckinDetails from '../../models/CheckinDetails';
 
 const Map = styled.div`
     height: 100%;
@@ -11,9 +12,11 @@ interface GoogleProps {
 }
 
 interface GoogleState {
-    google?: typeof google;
+    hasGoogleLoaded: boolean;
     mapRef: React.RefObject<HTMLDivElement>;
 }
+
+type PlacesService = google.maps.places.PlacesService;
 
 class Google extends React.Component<GoogleProps, GoogleState> {
     private map?: google.maps.Map = undefined;
@@ -22,7 +25,7 @@ class Google extends React.Component<GoogleProps, GoogleState> {
         super(props);
         window.googleInit = this.googleInit;
         this.state = {
-            google: window.hasGoogleLoaded ? google : undefined,
+            hasGoogleLoaded: window.hasGoogleLoaded,
             mapRef: React.createRef<HTMLDivElement>(),
         };
     }
@@ -45,69 +48,96 @@ class Google extends React.Component<GoogleProps, GoogleState> {
 
     private googleInit = () => {
         console.log('google init in component');
-        this.setState({ google });
+        this.setState({ hasGoogleLoaded: true });
     }
 
     private createMapIfNecessary = () => {
         // bounce if map already exists, or google is not initialized
-        if (this.map || !this.state.google) { return; }
+        if (this.map || !this.state.hasGoogleLoaded) { return; }
         console.log('creating map...');
 
-        const maps = this.state.google.maps;
         const node = this.state.mapRef.current;
-
         if (node instanceof HTMLDivElement) {
             // this.map = new maps.Map(node, {
             //     disableDefaultUI: true,
             //     draggable: false,
             //     disableDoubleClickZoom: true,
             // });
-            this.map = new maps.Map(node);
+            this.map = new google.maps.Map(node);
         }
     }
 
     private updateMapIfNecessary = () => {
         // bounce if map or google doesn't exist
-        if (!this.map || !this.state.google) { return; }
+        if (!this.map || !this.state.hasGoogleLoaded) { return; }
         console.log('updating map...');
 
-        const maps = this.state.google.maps;
         const { checkins } = this.props;
+        const service = new google.maps.places.PlacesService(this.map);
 
-        const markers = checkins.map((checkin) => {
-            const location = new maps.LatLng(checkin.location.lat, checkin.location.long);
-            const place = {
-                location,
-                query: checkin.venueName,
-            };
-            const marker = new maps.Marker({
-                title: checkin.venueName,
-                position: location,
-                map: this.map,
-                place,
+        const detailPromises = checkins.map((c) => this.fetchPlace(c, service));
+        Promise.all(detailPromises).then((allDetails) => {
+            return allDetails.map((details): google.maps.Marker|null => {
+                if (!details) { return null; }
+                const position = new google.maps.LatLng(details.lat, details.long);
+                const marker = new google.maps.Marker({
+                    title: details.name,
+                    position,
+                    map: this.map,
+                });
+                marker.addListener('click', () => {
+                    console.log(`click: ${details.name}`);
+                });
+                marker.addListener('mouseover', () => {
+                    console.log(`mouseover: ${details.name}`);
+                });
+                marker.addListener('mouseout', () => {
+                    console.log(`mouseout: ${details.name}`);
+                });
+                return marker;
             });
-            marker.addListener('click', () => {
-                console.log(`click: ${checkin.venueName}`);
-            });
-            marker.addListener('mouseover', () => {
-                console.log(`mouseover: ${checkin.venueName}`);
-            });
-            marker.addListener('mouseout', () => {
-                console.log(`mouseout: ${checkin.venueName}`);
-            });
-            return marker;
-        });
-        this.fitMapBoundsToMarkers(markers);
+        }).then((markers) => this.fitMapBoundsToMarkers(markers.filter(Boolean) as any));
     }
 
     private fitMapBoundsToMarkers = (markers: google.maps.Marker[]) => {
-        if (!this.map || !this.state.google) { return; }
-        const maps = this.state.google.maps;
-        const bounds = new maps.LatLngBounds();
+        if (!this.map || !this.state.hasGoogleLoaded) { return; }
+        const bounds = new google.maps.LatLngBounds();
         markers.forEach((marker) => {
             bounds.extend(marker.getPosition());
         });
         this.map.fitBounds(bounds);
+    }
+
+    private fetchPlace = async (checkin: Checkin, service: PlacesService): Promise<CheckinDetails|null> => {
+        return new Promise((resolve, reject) => {
+            service.findPlaceFromQuery({
+                query: checkin.venueName,
+                fields: ['formatted_address', 'geometry', 'photos'],
+                locationBias: new google.maps.LatLng(checkin.location.lat, checkin.location.long),
+            }, (results, status) => {
+                if (status !== google.maps.places.PlacesServiceStatus.OK || results.length < 1) {
+                    resolve(null);
+                }
+
+                const result = results[0];
+                let photoURL = undefined as (string|undefined); // is there a more idiomatic way?
+                if (result.photos.length > 0) {
+                    photoURL = result.photos[0].getUrl({});
+                }
+
+                const details: CheckinDetails = {
+                    name: checkin.venueName,
+                    lat: result.geometry.location.lat(),
+                    long: result.geometry.location.lng(),
+                    address: result.formatted_address,
+                    dateString: 'todo', // make me pretty
+                    linkURL: result.url,
+                    photoURL,
+                    stickerImageURL: checkin.imageURL,
+                };
+                resolve(details);
+            });
+        });
     }
 }
 
