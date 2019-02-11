c49e025a1cb8009c8de20f217ca410d7ac8ed04c
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-02-11 08:17:50 -0500

    Add media query inside Games component

--- src/components/Games/Games.tsx
+++ src/components/Games/Games.tsx
@@ -1,17 +1,31 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import styled from 'styled-components';
 import Header from '../shared/Header';
 
 const Game = styled.iframe`
+    display: block;
+    margin: auto;
     border-style: solid;
     border-width: thin;
 `;
 
-const Games: FunctionComponent = () => (
+interface WidthExpecting {
+    width: number;
+}
+
+const GamesMediaQuery = () => {
+    const mediaQuery = window.matchMedia('(max-width: 700px)');
+    const widthFromMQ = (mq: MediaQueryList|MediaQueryListEvent) => mq.matches ? 354 : 552;
+    const [width, setWidth] = useState<number>(widthFromMQ(mediaQuery));
+    mediaQuery.addListener((mq) => setWidth(widthFromMQ(mq)));
+    return <Games width={width} />;
+};
+
+const Games: FunctionComponent<WidthExpecting> = ({ width }) => (
     <div className="games">
         <Header>Games</Header>
-        <Game src="https://itch.io/embed/359750" width="552" height="167"></Game>
+        <Game src="https://itch.io/embed/359750" width={width} height="167"></Game>
     </div>
 );
 
-export default Games;
+export default GamesMediaQuery;
